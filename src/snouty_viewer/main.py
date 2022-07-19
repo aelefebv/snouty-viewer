Fix pixel scaling on traditional view

src/snouty_viewer/main.py
@@ -3,36 +3,37 @@ import sys
 import napari
 import numpy as np
 import im_container
-import skimage.transform
+import scipy.ndimage
 
 
-def interpolated_view(im: im_container.Im):
+def traditional_view(im: im_container.Im):  # a little smaller than native view, but much slower
     im_raw = im.load_raw()
+    im_translated = native_view(im)
     num_t, num_c, num_z, num_y, num_x = im_raw.shape
+    _, _, _, t_num_y, _ = im_translated.shape
+    rotation_angle = float(im.metadata['tilt'])
     z_ratio = float(im.metadata['voxel_aspect_ratio'])
-    scan_step_size_px = float(int(im.metadata['scan_step_size_px']))
-    shear_angle = np.arctan(z_ratio)
-    shear_length = int(np.rint(num_y * np.sin(shear_angle)))
-    y_shrink = int(np.rint(shear_length / np.tan((np.pi-shear_angle)/2)))
-    z_scaling = np.sqrt(scan_step_size_px**2+1)
-    tform = skimage.transform.AffineTransform(shear=-shear_angle, scale=(z_scaling, 1))
-    num_y_new = num_y - y_shrink
-    num_z_new = int(np.rint((shear_length + num_z*z_scaling)))
-    im_out = np.zeros((num_t, num_c, num_y_new, num_z_new, num_x), dtype=im_raw.dtype)
-    swapped = np.swapaxes(im_raw, 2, 3)
+
+    scaled_z = num_z * z_ratio
+    new_y = int(np.rint(scaled_z*np.sin(rotation_angle) + t_num_y*np.cos(rotation_angle)))
+    new_z = int(np.rint(num_y*np.sin(rotation_angle)))
+    temp_z = int(np.rint(scaled_z*np.cos(rotation_angle) + t_num_y*np.sin(rotation_angle)))
+    start_z = (temp_z-new_z)//2
+
+    im_out = np.zeros((num_t, num_c, new_z, new_y, num_x), dtype=im_raw.dtype)
     for t in range(num_t):
         sys.stdout.write(f"\r[INFO] Interpolating volume {t+1} of {num_t}...")
         sys.stdout.flush()
         for c in range(num_c):
-            for x in range(num_x):
-                im_out[t, c, :, :, x] = skimage.transform.warp(swapped[t, c, :, :, x], tform.inverse,
-                                                               preserve_range=True,
-                                                               output_shape=(num_y_new, num_z_new),
-                                                               order=3)  # order 3 seems to be fastest
+            temp1 = scipy.ndimage.zoom(im_translated[t, c, ...], zoom=(z_ratio, 1, 1),
+                                       order=1, prefilter=True)
+            temp2 = scipy.ndimage.rotate(temp1, np.rad2deg(rotation_angle),
+                                         order=1, prefilter=True, reshape=True)
+            im_out[t, c, ...] = temp2[start_z:(start_z+new_z), ...]
     return im_out
 
 
-def translated_view(im: im_container.Im):
+def native_view(im: im_container.Im):  # a little bigger than traditional view, but much faster
     im_raw = im.load_raw()
     num_t, num_c, num_z, num_y, num_x = im_raw.shape
     scan_step_size_px = int(im.metadata['scan_step_size_px'])
@@ -48,18 +49,17 @@ if __name__ == "__main__":
     TOP_DIR = "/Users/austin/test_files/snouty_raw/2022-04-21_16-52-33_000_mitotracker_ER-mEmerald/"
     IM_NAME = "000000"
 
-    im = im_container.Im(TOP_DIR, IM_NAME)
+    im_info = im_container.Im(TOP_DIR, IM_NAME)
 
-    im_raw = im.load_raw()
-    im_preview = im.load_preview()
-    im_translated = translated_view(im)
-    im_interpolated = interpolated_view(im)
+    im_original = im_info.load_raw()
+    im_preview = im_info.load_preview()
+    im_native = native_view(im_info)
+    im_traditional = traditional_view(im_info)
 
-    z_ratio = float(im.metadata['voxel_aspect_ratio'])
-    scale = (z_ratio, 1, 1)
+    scale = (float(im_info.metadata['voxel_aspect_ratio']), 1, 1)
 
     viewer = napari.Viewer()
-    viewer.add_image(im_raw[:, 0, ...], scale=scale, contrast_limits=(0, 2000), colormap='viridis')
-    viewer.add_image(im_translated[:, 0, ...], scale=scale, contrast_limits=(0, 2000), colormap='viridis')
-    viewer.add_image(im_interpolated[:, 0, ...], contrast_limits=(0, 2000), colormap='viridis')
+    viewer.add_image(im_original[:, 0, ...]+1, scale=scale, colormap='viridis', name='original')
+    viewer.add_image(im_native[:, 0, ...]+1, scale=scale, colormap='viridis', name='native')
+    viewer.add_image(im_traditional[:, 0, ...]+1, colormap='viridis', name='traditional')
     napari.run()