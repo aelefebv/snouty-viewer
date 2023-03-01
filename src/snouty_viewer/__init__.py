b603e3a834f71298ba17c66dfe4ec1f82dbabef7
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-03-01 11:35:08 -0800

    Update requirements

--- src/snouty_viewer/__init__.py
+++ src/snouty_viewer/__init__.py
@@ -1,9 +1,6 @@
-__version__ = "0.0.1"
+__version__ = "0.2.1"
 
 from ._reader import napari_get_reader
 from ._widget import native_view
 
-__all__ = (
-    "napari_get_reader",
-    "native_view"
-)
+__all__ = ("napari_get_reader", "native_view")
