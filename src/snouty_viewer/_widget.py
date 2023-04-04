5d85d9d6fb1911a65a909f2a307d162e87ed8a47
Austin E. Y. T. Lefebvre <austin.e.lefebvre@gmail.com>
2023-04-04 13:35:57 -0700

    WIP no loading channel into memory

--- src/snouty_viewer/_widget.py
+++ src/snouty_viewer/_widget.py
@@ -7,8 +7,11 @@ import napari.types
 import numpy as np
 from magicgui import magic_factory
 
-from snouty_viewer._writer import write_single_image
-from snouty_viewer.im_loader import ImPathInfo, load_full
+from snouty_viewer.im_loader import (
+    ImPathInfo,
+    allocate_memory_return_memmap,
+    load_full,
+)
 
 
 class PseudoImage:
@@ -20,30 +23,41 @@ class PseudoImage:
 
 
 class ImInfo:
-    def __init__(self, im):
+    def __init__(self, im, im_shape=None):
+        if im_shape is None:
+            im_shape = im.shape
         (
             self.num_t,
             self.num_c,
             self.num_z,
             self.num_y,
             self.num_x,
-        ) = im.data.shape
+        ) = im_shape
         self.scan_step_size_px = int(
             im.metadata["snouty_metadata"]["scan_step_size_px"]
         )
         self.max_deshear_shift = int(
             np.rint(self.scan_step_size_px * (self.num_z - 1))
         )
-        self.im_desheared = np.zeros(
-            (
-                self.num_t,
-                self.num_c,
-                self.num_z,
-                self.num_y + self.max_deshear_shift,
-                self.num_x,
-            ),
-            im.dtype,
+        self.im_desheared_shape = (
+            self.num_t,
+            self.num_c,
+            self.num_z,
+            self.num_y + self.max_deshear_shift,
+            self.num_x,
         )
+        self.im_desheared = None
+        # get desheared image memmap
+        # self.im_desheared = np.zeros(
+        #     (
+        #         self.num_t,
+        #         self.num_c,
+        #         self.num_z,
+        #         self.num_y + self.max_deshear_shift,
+        #         self.num_x,
+        #     ),
+        #     im.dtype,
+        # )
         self.px_size = float(im.metadata["snouty_metadata"]["sample_px_um"])
         self.z_px_size = self.px_size * float(
             im.metadata["snouty_metadata"]["voxel_aspect_ratio"]
@@ -58,22 +72,52 @@ class ImInfo:
         self.name = im.name
         self.displayed_images = []
 
+    # def _allocate_memory(self,
+    #                      path_im: str,
+    #                      dtype: Union[Type, str] = 'float', data = None,
+    #                      shape: tuple = None,
+    #                      description: str = 'No description.'):
+    #     axes = 'TCZYX'
+    #     if shape is None:
+    #
+    #
+    #     if data is None:
+    #         assert shape is not None
+    #         tifffile.imwrite(
+    #             path_im, shape=shape, dtype=dtype, bigtiff=True,
+    #             metadata={"axes": axes}
+    #         )
+    #     else:
+    #         tifffile.imwrite(
+    #             path_im, data, bigtiff=True, metadata={"axes": axes}
+    #         )
+    #     ome_xml = tifffile.tiffcomment(path_im)
+    #     ome = ome_types.from_xml(ome_xml, parser="lxml")
+    #     ome.images[0].pixels.physical_size_x = self.dim_sizes['X']
+    #     ome.images[0].pixels.physical_size_y = self.dim_sizes['Y']
+    #     ome.images[0].pixels.physical_size_z = self.dim_sizes['Z']
+    #     ome.images[0].pixels.time_increment = self.dim_sizes['T']
+    #     ome.images[0].description = description
+    #     ome.images[0].pixels.type = dtype
+    # note: numpy uses 8 bits as smallest, so 'bit' type does nothing for bool.
+    #     ome_xml = ome.to_xml()
+    #     tifffile.tiffcomment(path_im, ome_xml)
+
     def _deshear_channel(self, ch_num):
-        desheard_ch = np.zeros(
-            (
-                self.num_t,
-                1,
-                self.num_z,
-                self.num_y + self.max_deshear_shift,
-                self.num_x,
-            ),
-            self.dtype,
-        )
-        for z in range(self.num_z):
-            deshear_shift = int(np.rint(z * self.scan_step_size_px))
-            desheard_ch[
-                :, 0, z, deshear_shift : (deshear_shift + self.num_y), :
-            ] = self.data[:, ch_num, z, :, :]
+        if self.num_c == 1:
+            desheard_ch = self.im_desheared
+            for z in range(self.num_z):
+                deshear_shift = int(np.rint(z * self.scan_step_size_px))
+                desheard_ch[
+                    :, z, deshear_shift : (deshear_shift + self.num_y), :
+                ] = self.data[:, z, :, :]
+        else:
+            desheard_ch = self.im_desheared[:, ch_num, :, :, :]
+            for z in range(self.num_z):
+                deshear_shift = int(np.rint(z * self.scan_step_size_px))
+                desheard_ch[
+                    :, z, deshear_shift : (deshear_shift + self.num_y), :
+                ] = self.data[:, ch_num, z, :, :]
         return desheard_ch
 
     def _display_image(
@@ -111,8 +155,13 @@ class ImInfo:
 
     def deshear_all_channels(self, batch=False, show_multi=False):
         for ch in range(self.num_c):
-            wavelength = int(self.wavelengths[ch])
-            if wavelength < 430:
+            try:
+                wavelength = int(self.wavelengths[ch])
+            except ValueError:
+                wavelength = 0
+            if wavelength == 0:
+                color = "gray"
+            elif wavelength < 430:
                 color = "bop purple"
             elif wavelength < 480:
                 color = "blue"
@@ -130,8 +179,8 @@ class ImInfo:
                 color = "magenta"
             ch_desheared = self._deshear_channel(ch)
             if self.num_c > 1:
-                self.im_desheared[:, ch, ...] = ch_desheared[:, 0, ...]
-                ch_desheared = ch_desheared[:, 0, ...]
+                self.im_desheared[:, ch, ...] = ch_desheared[:, ...]
+                ch_desheared = ch_desheared[:, ...]
             else:
                 self.im_desheared = ch_desheared
             if not batch:
@@ -162,23 +211,38 @@ def list_subdirectories(path):
 
 @magic_factory(call_button="Deskew and save")
 def batch_deskew_and_save(
-    path: str, show_deskewed_ims: bool = False, auto_save: bool = True
+    path_in: str,
+    path_out: str = "",
+    show_deskewed_ims: bool = False,
+    auto_save: bool = True,
 ) -> Union[List[napari.types.LayerDataTuple], None]:
-    snouty_dirs = list_subdirectories(path)
+    snouty_dirs = list_subdirectories(path_in)
     tuple_list = []
     for snouty_dir in snouty_dirs:
+        if path_out == "":
+            path_out = snouty_dir
         im_path_info = ImPathInfo(snouty_dir)
-        loaded_im = PseudoImage(load_full(im_path_info)[0])
-        im_info = ImInfo(loaded_im)
+        skewed_memmap = PseudoImage(load_full(im_path_info, path_out)[0])
+        im_info = ImInfo(skewed_memmap, im_path_info.im_shape)
+        name = im_path_info.path.rsplit(os.sep)[-1]
+        save_path = os.path.join(path_out, f"deskewed-{name}.ome.tif")
+        deskewed_memmap = allocate_memory_return_memmap(
+            "TCZYX",
+            im_info.im_desheared_shape,
+            im_path_info.metadata,
+            save_path,
+            im_info.dtype,
+        )
+        im_info.im_desheared = deskewed_memmap
         im_info.deshear_all_channels(batch=True, show_multi=show_deskewed_ims)
         if show_deskewed_ims:
             tuple_list.append(im_info.displayed_images[0])
-        if auto_save:
-            save_path = os.path.join(
-                snouty_dir, f"deskewed-{im_info.name}.ome.tif"
-            )
-            attributes = {"metadata": im_info.metadata}
-            write_single_image(save_path, im_info.im_desheared, attributes)
+        # if auto_save:
+        #     save_path = os.path.join(
+        #         path_out, f"deskewed-{im_info.name}.ome.tif"
+        #     )
+        #     attributes = {"metadata": im_info.metadata}
+        #     write_single_image(save_path, im_info.im_desheared, attributes)
 
     if show_deskewed_ims:
         return tuple_list
